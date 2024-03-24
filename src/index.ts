5f1df557ae1bc1c7babe250104832112c852b0af
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-03-24 15:46:59 -0300

    Added config parsing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('demo')
+import * as console from 'console';
+import { readFile, mkdir, unlink } from 'node:fs/promises';
+import { join } from 'node:path';
+import { parse } from 'yaml';
+
+const projectPath = '/Users/evg4b/Desktop/go-package';
+const buildName = 'go-package';
+
+(async () => {
+  const configPath = join(projectPath, 'dist', 'config.yaml');
+
+  const file = await readFile(configPath, 'utf8');
+  const { project_name, builds } = parse(file) as GoReleaserConfiguration;
+  const targetBuild = builds?.find((build: any) => build.id === buildName);
+  await unlink(join(projectPath, 'dist', 'npm')).catch(() => {});
+  await mkdir(join(projectPath, 'dist', 'npm'), { recursive: true });
+
+  console.log(project_name, targetBuild);
+})();
