06d211fcff314832bf12118aba418b4ae0f130c0
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-03-26 18:18:29 -0300

    Added yargs

---
+++ src/commands/build.ts
@@ -0,0 +1,67 @@
+import console from 'console';
+import { copyFile, mkdir, unlink } from 'node:fs/promises';
+import { join, sep } from 'node:path';
+import { parse as parsePath } from 'path';
+import { ArgumentsCamelCase, Argv, BuilderCallback } from 'yargs';
+import { parseArtifactsFile, parseMetadata } from '../files';
+import { writePackage } from '../files/package';
+import { Context } from '../gorealiser/context';
+import { transformPackage } from '../package/transform';
+
+const builder: BuilderCallback<unknown, unknown> = (yargs): Argv<{ path: string }> => {
+    return yargs.positional('path', {
+        type: 'string',
+        describe: 'Build the project',
+        default: '.',
+        conflicts: 'version',
+    });
+};
+
+const projectPath = '/Users/evg4b/Desktop/go-package';
+const buildName = 'go-package';
+
+
+const handler: ((args: ArgumentsCamelCase<{ path: string }>) => (void | Promise<void>)) = async (args) => {
+    console.log('Building the project...', args.path);
+
+    const context = new Context(projectPath);
+
+    const artifactsData = await parseArtifactsFile(context.artifactsPath);
+    const metadata = await parseMetadata(context.metadataPath);
+
+
+    await unlink(context.distPath).catch(() => {});
+    await mkdir(context.distPath, { recursive: true });
+
+    const artifacts = artifactsData.filter(({ extra, type }) => {
+        return extra.ID === buildName && type === 'Binary';
+    });
+
+    for (const artifact of artifacts) {
+        const pathItems = artifact.path.split(sep);
+        const { path } = artifact;
+        const sourceArtifactPath = join(projectPath, path);
+        const { base } = parsePath(path);
+        const npmArtifactPath = context.packageFolder(pathItems[1]);
+        await mkdir(npmArtifactPath, { recursive: true });
+        const npmArtifact = join(npmArtifactPath, base);
+        const packageDefinition = transformPackage(artifact, metadata);
+
+        console.log('Package:', packageDefinition);
+
+
+        await copyFile(sourceArtifactPath, npmArtifact);
+        await writePackage(context.packageJson(pathItems[1]), packageDefinition);
+    }
+
+    // console.log('Project name:', project_name);
+    // console.log('Target build:', targetBuild);
+    // console.log('Artifacts:', artifacts);
+
+    // console.log(project_name, targetBuild, artifacts);
+};
+
+export const buildCommand = {
+    builder: builder,
+    handler: handler,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,8 @@
-import * as console from 'console';
-import { copyFile, mkdir, unlink } from 'node:fs/promises';
-import { join, parse as parsePath, sep } from 'node:path';
-import { parseArtifactsFile, parseMetadata } from './files';
-import { writePackage } from './files/package';
-import { Context } from './gorealiser/context';
-import { transformPackage } from './package/transform';
+import yargs from 'yargs';
+import { buildCommand } from './commands';
 
-const projectPath = '/Users/evg4b/Desktop/go-package';
-const buildName = 'go-package';
-
-(async () => {
-    const context = new Context(projectPath);
-
-    const artifactsData = await parseArtifactsFile(context.artifactsPath);
-    const metadata = await parseMetadata(context.metadataPath);
-
-
-    await unlink(context.distPath).catch(() => {});
-    await mkdir(context.distPath, { recursive: true });
-
-    const artifacts = artifactsData.filter(({ extra, type }) => {
-        return extra.ID === buildName && type === 'Binary';
-    });
-
-    for (const artifact of artifacts) {
-        const pathItems = artifact.path.split(sep);
-        const { path } = artifact;
-        const sourceArtifactPath = join(projectPath, path);
-        const { base } = parsePath(path);
-        const npmArtifactPath = context.packageFolder(pathItems[1]);
-        await mkdir(npmArtifactPath, { recursive: true });
-        const npmArtifact = join(npmArtifactPath, base);
-        const packageDefinition = transformPackage(artifact, metadata);
-
-        console.log('Package:', packageDefinition);
-
-
-        await copyFile(sourceArtifactPath, npmArtifact);
-        await writePackage(context.packageJson(pathItems[1]), packageDefinition);
-    }
-
-    // console.log('Project name:', project_name);
-    // console.log('Target build:', targetBuild);
-    // console.log('Artifacts:', artifacts);
-
-    // console.log(project_name, targetBuild, artifacts);
-})();
+yargs.scriptName('goreleaser-npm-publisher')
+    .usage('$0 <cmd> [args]')
+    .command('build', 'Build the project', buildCommand.builder, buildCommand.handler)
+    .help()
+    .argv;
