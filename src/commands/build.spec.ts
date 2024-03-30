4f195e27015ce157c262dd26bf1732f275b007f5
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-03-30 20:11:17 -0300

    Added index script generation

---
+++ src/commands/build.spec.ts
@@ -0,0 +1,132 @@
+import { js } from './build';
+
+describe('js transformer', () => {
+  it('should render code without interpolation', () => {
+    const actual = js`const path = require('path');`.toString();
+
+    expect(actual).toEqual('const path = require(\'path\');');
+  });
+
+  describe('should correctly render primitives', () => {
+    const cases = [
+      { name: 'string', value: 'string', expected: 'const value = \'string\';' },
+      { name: 'string', value: '', expected: 'const value = \'\';' },
+      { name: 'integer number', value: 123, expected: 'const value = 123;' },
+      { name: 'decimal number', value: 123.123123, expected: 'const value = 123.123123;' },
+      { name: 'negative integer number', value: -123, expected: 'const value = -123;' },
+      { name: 'negative decimal number', value: -123.123123, expected: 'const value = -123.123123;' },
+      { name: 'boolean', value: true, expected: 'const value = true;' },
+      { name: 'boolean', value: false, expected: 'const value = false;' },
+      { name: 'null', value: null, expected: 'const value = null;' },
+      { name: 'undefined', value: undefined, expected: 'const value = undefined;' },
+      { name: 'NaN', value: NaN, expected: 'const value = NaN;' },
+      { name: 'Infinity', value: Infinity, expected: 'const value = Infinity;' },
+      { name: 'negative Infinity', value: -Infinity, expected: 'const value = -Infinity;' },
+      {
+        name: 'Date',
+        value: new Date('2024-03-30T22:06:29.027Z'),
+        expected: 'const value = new Date(2024, 2, 30, 19, 6, 29, 27);',
+      },
+      { name: 'Symbol', value: Symbol('test-symbol'), expected: 'const value = Symbol(\'test-symbol\');' },
+    ];
+
+    cases.forEach(({ name, value, expected }) => {
+      it(name, () => {
+        const actual = js`const value = ${ value };`.toString();
+
+        expect(actual).toEqual(expected);
+      });
+    });
+  });
+
+  describe('should correctly render objects', () => {
+    const cases = [
+      { name: 'empty object', value: {}, expected: 'const value = {};' },
+      { name: 'object with string property', value: { key: 'value' }, expected: 'const value = { key: \'value\' };' },
+      { name: 'object with number property', value: { key: 123 }, expected: 'const value = { key: 123 };' },
+      { name: 'object with boolean property', value: { key: true }, expected: 'const value = { key: true };' },
+      { name: 'object with null property', value: { key: null }, expected: 'const value = { key: null };' },
+      {
+        name: 'object with undefined property',
+        value: { key: undefined },
+        expected: 'const value = { key: undefined };',
+      },
+      { name: 'object with NaN property', value: { key: NaN }, expected: 'const value = { key: NaN };' },
+      { name: 'object with Infinity property', value: { key: Infinity }, expected: 'const value = { key: Infinity };' },
+      {
+        name: 'object with negative Infinity property',
+        value: { key: -Infinity },
+        expected: 'const value = { key: -Infinity };',
+      },
+      {
+        name: 'object with Date property',
+        value: { key: new Date('2024-03-30T22:06:29.027Z') },
+        expected: 'const value = { key: new Date(2024, 2, 30, 19, 6, 29, 27) };',
+      },
+      {
+        name: 'object with Symbol property',
+        value: { key: Symbol('test-symbol') },
+        expected: 'const value = { key: Symbol(\'test-symbol\') };',
+      },
+      {
+        name: 'object with nested object',
+        value: { key: { nested: 'value' } },
+        expected: 'const value = { key: { nested: \'value\' } };',
+      },
+
+    ];
+
+    cases.forEach(({ name, value, expected }) => {
+      it(name, () => {
+        const actual = js`const value = ${ value };`.toString();
+
+        expect(actual).toEqual(expected);
+      });
+    });
+  });
+
+  describe('should correctly render arrays', () => {
+    const cases = [
+      { name: 'empty array', value: [], expected: 'const value = [];' },
+      { name: 'array with string element', value: ['value'], expected: 'const value = [ \'value\' ];' },
+      { name: 'array with number element', value: [123], expected: 'const value = [ 123 ];' },
+      { name: 'array with boolean element', value: [true], expected: 'const value = [ true ];' },
+      { name: 'array with null element', value: [null], expected: 'const value = [ null ];' },
+      {
+        name: 'array with undefined element',
+        value: [undefined],
+        expected: 'const value = [ undefined ];',
+      },
+      { name: 'array with NaN element', value: [NaN], expected: 'const value = [ NaN ];' },
+      { name: 'array with Infinity element', value: [Infinity], expected: 'const value = [ Infinity ];' },
+      {
+        name: 'array with negative Infinity element',
+        value: [-Infinity],
+        expected: 'const value = [ -Infinity ];',
+      },
+      {
+        name: 'array with Date element',
+        value: [new Date('2024-03-30T22:06:29.027Z')],
+        expected: 'const value = [ new Date(2024, 2, 30, 19, 6, 29, 27) ];',
+      },
+      {
+        name: 'array with Symbol element',
+        value: [Symbol('test-symbol')],
+        expected: 'const value = [ Symbol(\'test-symbol\') ];',
+      },
+      {
+        name: 'array with nested array',
+        value: [[1, 2, 3]],
+        expected: 'const value = [ [ 1, 2, 3 ] ];',
+      },
+    ];
+
+    cases.forEach(({ name, value, expected }) => {
+      it(name, () => {
+        const actual = js`const value = ${ value };`.toString();
+
+        expect(actual).toEqual(expected);
+      });
+    });
+  });
+});
--- src/commands/build.ts
+++ src/commands/build.ts
@@ -1,11 +1,11 @@
-import { copyFile, mkdir } from 'node:fs/promises';
+import { isEmpty } from 'lodash';
+import { copyFile, mkdir, writeFile } from 'node:fs/promises';
 import { join, sep } from 'node:path';
 import { parse as parsePath } from 'path';
 import { ArgumentsCamelCase } from 'yargs';
 import { parseArtifactsFile, parseMetadata, writePackage } from '../files';
 import { Context } from '../gorealiser';
 import { formatMainPackageJson, formatPackageJson, transformPackage } from '../package';
-
 
 export const buildHandler: ((args: ArgumentsCamelCase<DefaultParams>) => (void | Promise<void>)) = async (args) => {
   const context = new Context(args.project);
@@ -37,5 +37,82 @@ export const buildHandler: ((args: ArgumentsCamelCase<DefaultParams>) => (void |
   const packageJsonObject = formatMainPackageJson(packages, metadata, args.prefix);
   await mkdir(context.packageFolder(metadata.project_name), { recursive: true });
   await writePackage(context.packageJson(metadata.project_name), packageJsonObject);
-
+  await writeFile(
+    join(context.packageFolder(metadata.project_name), 'index.js'),
+    buildExecScript(packages, args.prefix),
+    'utf-8',
+  );
 };
+
+const buildExecScript = (packages: PackageDefinition[], prefix: string | undefined) => {
+  const mapping = packages.reduce((acc, pkg) => {
+    const data = isEmpty(prefix) ? [pkg.name] : [prefix!, pkg.name];
+    return { ...acc, [`${ pkg.os }_${ pkg.cpu }`]: [...data, pkg.bin] };
+  }, {} as Record<string, string[]>);
+
+  const directory = isEmpty(prefix)
+    ? js`path.dirname(__dirname)`
+    : js`path.dirname(path.dirname(__dirname))`;
+
+  return js`const path = require('path');
+const child_process = require('child_process');
+const mapping = ${ mapping };
+const modulesDirectory = ${ directory };
+const definition = mapping[process.platform + '_' + process.arch];
+const packagePath = path.join(modulesDirectory, ...definition);
+child_process.spawn(packagePath, process.argv.splice(2), {
+    stdio: 'inherit',
+    env: process.env,
+});`;
+};
+
+
+const transform = (datum: unknown): string => {
+  switch (true) {
+  case Code.isCode(datum):
+    return datum.toString();
+  case typeof datum === 'string':
+    return `'${ datum }'`;
+  case typeof datum === 'number':
+    return datum.toString();
+  case typeof datum === 'boolean':
+    return datum ? 'true' : 'false';
+  case datum === null:
+    return 'null';
+  case datum === undefined:
+    return 'undefined';
+  case datum instanceof Date:
+    return `new Date(${ datum.getFullYear() }, ${ datum.getMonth() }, ${ datum.getDate() }, ${ datum.getHours() }, ${ datum.getMinutes() }, ${ datum.getSeconds() }, ${ datum.getMilliseconds() })`;
+  case typeof datum === 'symbol':
+    return `Symbol('${ datum.description }')`;
+  case Array.isArray(datum):
+    return !isEmpty(datum)
+      ? `[ ${ datum.map(transform).join(', ') } ]`
+      : '[]';
+  case typeof datum === 'object':
+    return !isEmpty(datum)
+      ? `{ ${ Object.entries(datum)
+        .map(([key, value]) => `${ key }: ${ transform(value) }`)
+        .join(', ') } }`
+      : '{}';
+  }
+
+  throw new Error(`Unsupported type: ${ typeof datum }`);
+};
+
+export const js = (templates: TemplateStringsArray, ...data: unknown[]): Code => {
+  let index = 0;
+  const jsCode = templates.reduce((code, codePart) => {
+    return code + transform(data[index++]) + codePart;
+  });
+
+  return new Code(jsCode);
+};
+
+class Code extends String {
+  private __code = true;
+
+  public static isCode(value: unknown): value is Code {
+    return value instanceof Code && value.__code;
+  }
+}
