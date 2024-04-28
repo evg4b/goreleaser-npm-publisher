6b29ac01b69b4b3f8e4a1a884b8d4c04496b8305
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-04-28 02:34:05 -0300

    Added init version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-(() => { throw new Error('Not implemented'); })();
+import { type ArgumentsCamelCase } from 'yargs';
+import { publishHandler } from './commands';
+
+type Params = ArgumentsCamelCase<Omit<DefaultParams, 'clear'>>;
+export default async (options: Params) => {
+  await publishHandler({ ...options, clear: true });
+};
